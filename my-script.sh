4cd511bc69994d1199d896c17a907177ed263ae3
gowthamsubburaj <97084573+gowthamsubburaj@users.noreply.github.com>
2022-01-04 12:48:01 +0530

    Initial commits

---
+++ my-script.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+read buildtype
+if [ "$buildtype" = "client" ]
+then
+   echo "buildtype this is client environment"
+   
+elif [ "$buildtype" = "admin" ]
+then
+   echo "buildtype this is admin environment"
+ 
+elif [ "$buildtype" = "backend" ]
+then
+   echo "buildtype this is backend  environment"
+else
+  echo invalid option
+
+fi
+
